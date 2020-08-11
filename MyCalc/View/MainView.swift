cd44e8aac8cef2c227b07844d5a81129d338aa02
RobertHan96 <yshan4329@gmail.com>
2020-08-11 22:27:35 +0900

    화면 방향에 따른 UI적용 코드 구현 중, 기기 방향 감지 로직까지 구현

--- MyCalc/View/MainView.swift
+++ MyCalc/View/MainView.swift
@@ -24,75 +24,125 @@ class MainView: MainViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupUI()
+        setDeviceOrientationNotificaition()
+        printDeviceScrrenInformations()
+    }
+    
+    func setDeviceOrientationNotificaition() {
+        var didRotate: (Notification) -> Void = { notification in
+            switch UIDevice.current.orientation {
+            case .landscapeLeft, .landscapeRight:
+                print("landscape")
+                self.setupUI(orientaion: "landscape")
+            case .portrait, .portraitUpsideDown:
+                print("Portrait")
+                self.setupUI(orientaion: "portrait")
+            default:
+                print("other")
+                self.setupUI(orientaion: "landscape")
+            }
+        }
+        
+        NotificationCenter.default.addObserver(forName: UIDevice.orientationDidChangeNotification,
+        object: nil,
+        queue: .main,
+        using: didRotate)
     }
 
-    func setupUI(){
+    func setupUI(orientaion : String){
         let buttons : [UIButton?] = [
             btnZero, btnOne, btnTwo, btnThree, btnFour ,btnFive, btnSix, btnSeven, btnEight, btnNine, btnRemainder, btnHistory, btnClear, btnMultiple,
             btnPlus, btnMinus, btnMultiple, btnEqual, btnPoint, btnDivision
         ]
-        let model = UIDevice.current.model
-        let deviceScrrenSize = UIScreen.main.bounds
-        print("[Log] 디바이스 정보",model, deviceScrrenSize)
-
-        let btnImageSize = btnHistory.bounds.height/4
-        btnHistory.imageEdgeInsets = UIEdgeInsets(top: btnImageSize, left: btnImageSize, bottom: btnImageSize, right: btnImageSize)
         
-        for button in buttons {
-            if let btn = button {
-                btn.setFontSizeDependOnScreenSize()
-                btn.layer.cornerRadius = btn.bounds.width/4
-                btn.titleLabel?.adjustsFontForContentSizeCategory = true
-                btn.titleLabel?.adjustsFontSizeToFitWidth = true
+        if orientaion == "landscape" {
+            let btnImageSize = btnHistory.bounds.height/4
+            
+            btnHistory.imageEdgeInsets = UIEdgeInsets(top: btnImageSize, left: btnImageSize, bottom: btnImageSize, right: btnImageSize)
+                    
+            for button in buttons {
+                if let btn = button {
+//                    btn.setFontSizeDependOnScreenSize()
+                    btn.layer.cornerRadius = 10
+                    btn.titleLabel?.adjustsFontForContentSizeCategory = true
+                    btn.titleLabel?.adjustsFontSizeToFitWidth = true
+                }
+            }
+
+        } else {
+            let btnImageSize = btnHistory.bounds.height/4
+            
+            btnHistory.imageEdgeInsets = UIEdgeInsets(top: btnImageSize, left: btnImageSize, bottom: btnImageSize, right: btnImageSize)
+                    
+            for button in buttons {
+                if let btn = button {
+                    btn.setFontSizeDependOnScreenSize()
+                    btn.layer.cornerRadius = btn.bounds.width/4
+                    btn.titleLabel?.adjustsFontForContentSizeCategory = true
+                    btn.titleLabel?.adjustsFontSizeToFitWidth = true
+                }
             }
         }
         
     }
     
+    
+    func printDeviceScrrenInformations() {
+        let model = UIDevice.current.model
+        let deviceScrrenSize = UIScreen.main.bounds
+        print("[Log] 디바이스 정보",model, deviceScrrenSize)
+    }
+    
+    func changeButtonsBoderRadius(buttons : [UIButton?]){
+        for button in buttons {
+            if let btn = button {
+                btn.layer.cornerRadius = 10
+            }
+        }
+    }
+    
 }
-
 
 extension UIButton {
     func setFontSizeDependOnScreenSize() {
         let bounds = UIScreen.main.bounds
         let height = bounds.size.height
-        let standardFontSize : CGFloat = 70
+        let standardFontSize : CGFloat = 60 // iphone11ProMax
         var calculatedFont = UIFont.systemFont(ofSize: standardFontSize)
-          switch height {
-          case 480.0: //Iphone 3,4S => 3.5 inch
-            calculatedFont = UIFont.systemFont(ofSize: 30)
-            resizeFont(calculatedFont: calculatedFont)
-            break
-          case 568.0: //iphone 5, SE => 4 inch
-            calculatedFont = UIFont.systemFont(ofSize: 30)
-            resizeFont(calculatedFont: calculatedFont)
-            break
-          case 667.0: //iphone 6, 6s, 7, 8 => 4.7 inch
-            calculatedFont = UIFont.systemFont(ofSize: 40)
-            resizeFont(calculatedFont: calculatedFont)
-            break
-          case 736.0: //iphone 6s+ 6+, 7+, 8+ => 5.5 inch
-            calculatedFont = UIFont.systemFont(ofSize: 50)
-            resizeFont(calculatedFont: calculatedFont)
-            break
-          case 812.0: //iphone X, XS => 5.8 inch
-            calculatedFont = UIFont.systemFont(ofSize: 70)
-            resizeFont(calculatedFont: calculatedFont)
-            break
-          case 896.0: //iphone XR => 6.1 inch  // iphone XS MAX => 6.5 inch
-            calculatedFont = UIFont.systemFont(ofSize: 70)
-            resizeFont(calculatedFont: calculatedFont)
-            break
-          default:
-            calculatedFont = UIFont.systemFont(ofSize: 100)
-            resizeFont(calculatedFont: calculatedFont)
-            break
-          }
+        
+        switch height {
+            case 480.0: //iphone 3,4S => 3.5 inch
+                calculatedFont = UIFont.systemFont(ofSize: standardFontSize * 0.5)
+                resizeFont(calculatedFont: calculatedFont)
+                break
+            case 568.0: //iphone 5, SE => 4 inch
+                calculatedFont = UIFont.systemFont(ofSize: standardFontSize * 0.6)
+                resizeFont(calculatedFont: calculatedFont)
+                break
+            case 667.0: //iphone 6, 6s, 7, 8 => 4.7 inch
+                calculatedFont = UIFont.systemFont(ofSize: standardFontSize * 0.7)
+                resizeFont(calculatedFont: calculatedFont)
+                break
+            case 736.0: //iphone 6s+ 6+, 7+, 8+ => 5.5 inch
+                calculatedFont = UIFont.systemFont(ofSize: standardFontSize * 0.8)
+                resizeFont(calculatedFont: calculatedFont)
+                break
+            case 812.0: //iphone X, XS => 5.8 inch
+                calculatedFont = UIFont.systemFont(ofSize: standardFontSize * 0.9)
+                resizeFont(calculatedFont: calculatedFont)
+                break
+            case 896.0: //iphone XR => 6.1 inch  // iphone XS MAX => 6.5 inch
+                calculatedFont = UIFont.systemFont(ofSize: standardFontSize)
+                resizeFont(calculatedFont: calculatedFont)
+                break
+            default:
+                calculatedFont = UIFont.systemFont(ofSize: standardFontSize * 1.8)
+                resizeFont(calculatedFont: calculatedFont)
+                break
         }
+    }
 
     func resizeFont(calculatedFont: UIFont?) {
         self.titleLabel?.font = calculatedFont
     }
-
 }
