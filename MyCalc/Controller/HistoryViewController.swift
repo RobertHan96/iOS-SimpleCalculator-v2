873fb9904d34f165850ecd03c298d45bbb157ab9
RobertHan96 <yshan4329@gmail.com>
2020-11-10 14:11:10 +0900

    히스토리 삭제 기능 추가

--- MyCalc/Controller/HistoryViewController.swift
+++ MyCalc/Controller/HistoryViewController.swift
@@ -1,22 +1,26 @@
-
 import UIKit
 import RealmSwift
 
-
-class HistoryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
+class HistoryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITableViewDataSourcePrefetching {
     @IBOutlet weak var labelTime: UILabel!
     @IBOutlet weak var labelResult: UILabel!
-    @IBOutlet weak var MyTableView: UITableView!
-    
+    @IBOutlet weak var historyTableVeiw: UITableView!
     let realm = try! Realm()
     var DateArrary : Results<CalcHistory>?
     
+    override func viewDidLoad() {
+        loadData()
+        super.viewDidLoad()
+    }
+
     func loadData() {
         DateArrary = realm.objects(CalcHistory.self).sorted(byKeyPath: "date", ascending: true)
-        self.MyTableView.reloadData()
+        self.historyTableVeiw.reloadData()
     }
     
+}
+
+extension HistoryViewController {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let realm = try! Realm()
         let realmCount = realm.objects(CalcHistory.self).count
@@ -24,7 +28,7 @@ class HistoryViewController: UIViewController, UITableViewDelegate, UITableViewD
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = self.MyTableView!.dequeueReusableCell(withIdentifier: "HistoryTabelViewCell", for: indexPath) as! HistoryTableViewCell
+        let cell = self.historyTableVeiw!.dequeueReusableCell(withIdentifier: "HistoryTabelViewCell", for: indexPath) as! HistoryTableViewCell
         cell.labelCalc.text = DateArrary?[indexPath.row].result
         cell.labelDate.text = DateArrary?[indexPath.row].date
         return cell
@@ -34,9 +38,23 @@ class HistoryViewController: UIViewController, UITableViewDelegate, UITableViewD
         return 100
     }
 
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        print("log prefetchRow \(IndexPath.self)")
+    }
     
-    override func viewDidLoad() {
-        loadData()
-        super.viewDidLoad()
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let deleteAction = UITableViewRowAction(style: .destructive, title: "Delete") { _, _ in
+            try? self.realm.write {
+                self.realm.delete((self.DateArrary?[indexPath.row])!)
+                self.historyTableVeiw.reloadData()
+            }
+
+        }
+        deleteAction.backgroundColor = .red
+        return [deleteAction]
     }
 }
